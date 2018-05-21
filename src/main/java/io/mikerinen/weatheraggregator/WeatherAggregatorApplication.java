8ef1d7371f6dd340805c58446e5dd6297cff748b
mikerinen <mike.m11@protonmail.com>
2018-05-21 21:47:06 +0200

    Format code and add dependencies

--- src/main/java/io/mikerinen/weatheraggregator/WeatherAggregatorApplication.java
+++ src/main/java/io/mikerinen/weatheraggregator/WeatherAggregatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherAggregatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherAggregatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAggregatorApplication.class, args);
+    }
 }
