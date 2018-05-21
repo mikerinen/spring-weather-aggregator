9e8f6fbc55a1547531ae1c6a58f2c0919e015768
mikerinen <mike.m11@protonmail.com>
2018-05-21 21:50:27 +0200

    Flyway database migration
    - add Flyway configuration
    - add the basic database schema
    - add test data to start

---
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE WEATHER_DATA (
+id                              IDENTITY NOT NULL PRIMARY KEY,
+location_name                   VARCHAR(50) NOT NULL,
+icon                            VARCHAR(50) NOT NULL,
+description                     VARCHAR(50) NOT NULL,
+long_description                VARCHAR(255) NOT NULL,
+air_pressure                    DOUBLE(17),
+humidity_percentage             DOUBLE(17),
+temperature_max                 DOUBLE(17),
+temperature_min                 DOUBLE(17),
+temperature                     DOUBLE(17),
+wind_speed_meters_per_second    DOUBLE(17),
+wind_direction_degrees          INTEGER(10),
+updated                         TIMESTAMP
+);
+
+CREATE INDEX idx_location_name on WEATHER_DATA (location_name);
