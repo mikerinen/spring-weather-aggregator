29bc7c57f20bb3e78d5055f9934ad1a9023bd574
mikerinen <mike.m11@protonmail.com>
2018-05-21 19:29:22 +0200

    Initial commit
    - add basic application structure
    - add dependencies
    - add description

---
+++ src/main/java/io/mikerinen/weatheraggregator/WeatherAggregatorApplication.java
@@ -0,0 +1,12 @@
+package io.mikerinen.weatheraggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAggregatorApplication.class, args);
+	}
+}
