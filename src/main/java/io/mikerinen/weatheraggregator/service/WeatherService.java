475a8838758d4865781433fd1dfd361185d25c1e
mikerinen <mike.m11@protonmail.com>
2018-06-19 21:53:37 +0200

    WeatherService
    - add YahooWeather to the combination
    - calculate the average for the temperature to be sent to the called
    - make tests deterministic by adding a fixed clock for time resolution

--- src/main/java/io/mikerinen/weatheraggregator/service/WeatherService.java
+++ src/main/java/io/mikerinen/weatheraggregator/service/WeatherService.java
@@ -3,6 +3,7 @@ package io.mikerinen.weatheraggregator.service;
 import io.mikerinen.weatheraggregator.entity.WeatherData;
 import io.mikerinen.weatheraggregator.repository.WeatherDataRepository;
 import io.mikerinen.weatheraggregator.webservice.openweathermap.OpenWeatherMapService;
+import io.mikerinen.weatheraggregator.webservice.yahoo.YahooWeatherService;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
@@ -20,17 +21,37 @@ public class WeatherService {
     @Resource(name = "openWeatherMapService")
     OpenWeatherMapService openWeatherMapService;
 
+    @Resource(name = "yahooWeatherService")
+    YahooWeatherService yahooWeatherService;
+
     public WeatherData getWeatherDataForLocation(String location) {
         Optional<WeatherData> weatherFromDB = Optional.ofNullable(weatherDataRepository.findByLocationName(location));
 
         if (weatherFromDB.isPresent()) {
             LocalDateTime twoHoursAgo = LocalDateTime.now().minusMinutes(120);
 
-            if (twoHoursAgo.isAfter(weatherFromDB.get().getUpdated()))
-                return openWeatherMapService.getWeatherDataForLocation(location);
-            else
+            if (twoHoursAgo.isAfter(weatherFromDB.get().getUpdated())) {
+                return fetchWeatherFromAPIs(location);
+                // TODO: assign a confidence score for the current weather
+                // TODO: save the temperature original values to a list
+            } else
                 return weatherFromDB.get();
         } else
-            return openWeatherMapService.getWeatherDataForLocation(location);
+            return fetchWeatherFromAPIs(location);
+    }
+
+    private WeatherData fetchWeatherFromAPIs(String location) {
+        WeatherData openWeatherMapData = openWeatherMapService.getWeatherDataForLocation(location);
+        WeatherData yahooWeatherData = yahooWeatherService.getWeatherDataForLocation(location);
+
+        yahooWeatherData.setLongDescription("OpenWeatherMap Temp: " + openWeatherMapData.getTemperature() + ", YahooWeather Temp: " + yahooWeatherData.getTemperature());
+
+        if (openWeatherMapData.getTemperature().equals(yahooWeatherData.getTemperature()))
+            yahooWeatherData.setTemperature(yahooWeatherData.getTemperature());
+        else {
+            yahooWeatherData.setTemperature(
+                    (openWeatherMapData.getTemperature() + yahooWeatherData.getTemperature()) / 2);
+        }
+        return yahooWeatherData;
     }
 }
--- src/test/java/io/mikerinen/weatheraggregator/service/WeatherServiceTest.java
+++ src/test/java/io/mikerinen/weatheraggregator/service/WeatherServiceTest.java
@@ -3,11 +3,15 @@ package io.mikerinen.weatheraggregator.service;
 import io.mikerinen.weatheraggregator.entity.WeatherData;
 import io.mikerinen.weatheraggregator.repository.WeatherDataRepository;
 import io.mikerinen.weatheraggregator.webservice.openweathermap.OpenWeatherMapService;
+import io.mikerinen.weatheraggregator.webservice.yahoo.YahooWeatherService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
@@ -21,29 +25,29 @@ public class WeatherServiceTest {
 
     private static final String TEST_LOCATION = "Helsinki";
 
+    private Clock fixedClock = Clock.fixed(Instant.parse("2000-01-01T00:00:00.00Z"), ZoneId.systemDefault());
+
     @Test
     public void testGetWeatherDataForLocationNotFoundInDB() {
         // setup
         WeatherDataRepository weatherDataRepositoryStub = mock(WeatherDataRepository.class);
         when(weatherDataRepositoryStub.findByLocationName(TEST_LOCATION)).thenReturn(null);
 
-        OpenWeatherMapService owmServiceStub = mock(OpenWeatherMapService.class);
-        WeatherData returnedWeatherData = new WeatherData();
-        returnedWeatherData.setId(100L);
-        returnedWeatherData.setUpdated(LocalDateTime.now());
-        when(owmServiceStub.getWeatherDataForLocation(TEST_LOCATION)).thenReturn(returnedWeatherData);
+        OpenWeatherMapService owmServiceStub = setupOpenWeatherMapServiceStub(LocalDateTime.now(fixedClock));
+        YahooWeatherService yahooWeatherServiceStub = setupYahooWeatherServiceStub(LocalDateTime.now(fixedClock));
 
         WeatherService weatherService = new WeatherService();
         weatherService.setOpenWeatherMapService(owmServiceStub);
         weatherService.setWeatherDataRepository(weatherDataRepositoryStub);
+        weatherService.setYahooWeatherService(yahooWeatherServiceStub);
 
         // execute
         WeatherData weatherData = weatherService.getWeatherDataForLocation(TEST_LOCATION);
 
         // verify
         assertThat(weatherData, is(not(nullValue())));
-        assertThat(weatherData.getId(), is(100L));
-        assertThat(weatherData.getUpdated(), is(not(nullValue())));
+        assertThat(weatherData.getId(), is(nullValue()));
+        assertThat(weatherData.getUpdated(), is(LocalDateTime.now(fixedClock)));
     }
 
     @Test
@@ -51,8 +55,10 @@ public class WeatherServiceTest {
         // setup
         WeatherDataRepository weatherDataRepositoryStub = setupWeatherRepoStub(LocalDateTime.MIN);
         OpenWeatherMapService owmServiceStub = setupOpenWeatherMapServiceStub(LocalDateTime.MAX);
+        YahooWeatherService yahooWeatherServiceStub = setupYahooWeatherServiceStub(LocalDateTime.MAX);
 
         WeatherService weatherService = new WeatherService();
+        weatherService.setYahooWeatherService(yahooWeatherServiceStub);
         weatherService.setOpenWeatherMapService(owmServiceStub);
         weatherService.setWeatherDataRepository(weatherDataRepositoryStub);
 
@@ -70,8 +76,10 @@ public class WeatherServiceTest {
         // setup
         WeatherDataRepository weatherDataRepositoryStub = setupWeatherRepoStub(LocalDateTime.MAX);
         OpenWeatherMapService owmServiceStub = setupOpenWeatherMapServiceStub(LocalDateTime.MIN);
+        YahooWeatherService yahooWeatherServiceStub = setupYahooWeatherServiceStub(LocalDateTime.MAX);
 
         WeatherService weatherService = new WeatherService();
+        weatherService.setYahooWeatherService(yahooWeatherServiceStub);
         weatherService.setOpenWeatherMapService(owmServiceStub);
         weatherService.setWeatherDataRepository(weatherDataRepositoryStub);
 
@@ -84,16 +92,30 @@ public class WeatherServiceTest {
         assertThat(weatherData.getUpdated(), is(LocalDateTime.MAX));
     }
 
-    private OpenWeatherMapService setupOpenWeatherMapServiceStub(LocalDateTime min) {
+    private OpenWeatherMapService setupOpenWeatherMapServiceStub(LocalDateTime time) {
         OpenWeatherMapService owmServiceStub = mock(OpenWeatherMapService.class);
 
         WeatherData returnedWeatherData = new WeatherData();
-        returnedWeatherData.setUpdated(min);
+        returnedWeatherData.setUpdated(time);
         returnedWeatherData.setId(null);
+        returnedWeatherData.setTemperature(1.0);
 
         when(owmServiceStub.getWeatherDataForLocation(TEST_LOCATION)).thenReturn(returnedWeatherData);
         return owmServiceStub;
     }
+
+    private YahooWeatherService setupYahooWeatherServiceStub(LocalDateTime time) {
+        YahooWeatherService yahooWeatherServiceStub = mock(YahooWeatherService.class);
+
+        WeatherData returnedWeatherData = new WeatherData();
+        returnedWeatherData.setUpdated(time);
+        returnedWeatherData.setId(null);
+        returnedWeatherData.setTemperature(2.0);
+
+        when(yahooWeatherServiceStub.getWeatherDataForLocation(TEST_LOCATION)).thenReturn(returnedWeatherData);
+        return yahooWeatherServiceStub;
+    }
+
 
     private WeatherDataRepository setupWeatherRepoStub(LocalDateTime updatedTime) {
         WeatherDataRepository weatherDataRepositoryStub = mock(WeatherDataRepository.class);
