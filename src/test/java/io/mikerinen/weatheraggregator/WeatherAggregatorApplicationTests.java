c242b3a41dfad4afaf038c6b5888739ec5faabb0
mikerinen <mike.m11@protonmail.com>
2018-06-19 20:54:59 +0200

    ApplicationTests
    - add contextLoads test with the assertion that the Controller is created

--- src/test/java/io/mikerinen/weatheraggregator/WeatherAggregatorApplicationTests.java
+++ src/test/java/io/mikerinen/weatheraggregator/WeatherAggregatorApplicationTests.java
@@ -1,16 +1,30 @@
 package io.mikerinen.weatheraggregator;
 
+import io.mikerinen.weatheraggregator.controller.WeatherController;
+import lombok.Setter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+@Setter
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherAggregatorApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
+    @Resource
+    private WeatherController controller;
+
+    @Test
+    public void contextLoads() {
+        assertThat(controller, is(notNullValue()));
+    }
 
 }
